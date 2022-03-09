713eb32d8be0c53b005afc4a6aa31553ae18b870
rascunhoDias <joao.bugad00@gmail.com>
2022-03-09 20:51:39 -0300

    init

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <!-- Compiled and minified CSS -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body >
+    
+<nav>
+    <div class="nav-wrapper">
+        <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    
+  
+        <ul id="slide-out" class="sidenav">
+          <li><a class="subheader">Subheader</a></li>
+          <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+        </ul>
+    </div>
+  </nav>
+
+
+  <div class="fixed-action-btn">
+  <a class="btn-floating btn-large red modal-trigger" data-target="modal1" >
+    <i class="material-icons">add</i>
+  </a>
+</div>
+
+<div id="modal1" class="modal bottom-sheet">
+    <div class="modal-content">
+      <h4>Add item</h4>
+      <form action="">
+        
+    
+      <div class="row">
+    <div class="input-field col s12">
+      <input id="first_name2" type="text" class="validate">
+      <label class="active" for="first_name2">Name</label>
+    </div>
+    <div class="input-field col s12">
+      <input id="first_name" type="number" class="validate">
+      <label class="active" for="first_name">Qtd</label>
+    </div>
+  </div>
+        
+      </form>
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+
+    </div>
+  </div>
+
+
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<script src="assets/js/main.js"></script>
+</body>
+
+</html>
